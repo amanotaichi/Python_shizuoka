356f81f86a9af02b596da2e23b3969d0df64702d
Haruki-Ito-digitech <ito.haruki.rc@mynavi.jp>
2024-05-14 06:50:56 +0000

    update

---
+++ Python/ito/inputname.py
@@ -0,0 +1,4 @@
+#名前の入力
+name = input("What's your name?")
+#表示
+print("Hello", name, "!")
--- Python/ito/mynameout.py
+++ Python/ito/mynameout.py
@@ -4,6 +4,8 @@ args = sys.argv
 
 #入力内容(引数)を変数に代入
 name = args[1]
-#表示
-print("Hello", name, "!", end="")
-
+try:
+    #表示
+    print("Hello", name, "!", end="")
+except:
+    print("入力がありません")
