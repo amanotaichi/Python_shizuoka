5e5829dce9620b27fbf5a80db99047b1a45f8276
Haruki-Ito-digitech <ito.haruki.rc@mynavi.jp>
2024-05-15 06:51:55 +0000

    タプル変更

--- Python/ito/distance.py
+++ Python/ito/distance.py
@@ -5,7 +5,13 @@ arg = sys.argv
 start = str(arg[1])
 end = str(arg[2])
 #駅名をキーに距離を値にしてタプル作成
-nozomi = {'東京': 0.00, '品川': 6.78, '新横浜': 25.54, '名古屋': 342.02, '京都': 476.31, '新大阪': 515.35}
+nozomi = {
+'東京': 0.00, 
+'品川': 6.78, 
+'新横浜': 25.54, 
+'名古屋': 342.02, 
+'京都': 476.31, 
+'新大阪': 515.35}
 nozomi_key = list(nozomi.keys())
 start_pos = nozomi_key.index(start)
 end_pos = nozomi_key.index(end)
