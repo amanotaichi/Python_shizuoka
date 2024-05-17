c6779f0374e017bf4c111cad09fdefefe512ace7
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-05-17 02:09:52 +0000

    update add comment pricedowd.py

--- Python/amano/pricedown.py
+++ Python/amano/pricedown.py
@@ -2,10 +2,12 @@ import sys
 
 # 引数入力
 args = sys.argv
-
-# 初期化
+# 引数を変数に代入
 cate = args[1]
 priced = int(args[2])
+
+# 初期化
+# 辞書を作成
 price = {
     "リンゴ":80 ,
     "みかん":198,
@@ -16,12 +18,12 @@ price = {
     "うどん":128,
     "パスタ":258
 }
-
+# カテゴリのリストを作成
 fruits = ["リンゴ", "みかん", "バナナ"]
 alcohol = ["ビール", "日本酒"]
 noodles = ["ラーメン", "うどん", "パスタ"]
 
-# 値下げ処理
+# カテゴリ別値下げ処理
 if cate == "果物類":
     for i in fruits:
         price[i] = price[i] - priced
@@ -33,9 +35,10 @@ elif cate == "酒類":
 elif cate == "麺類":
     for i in noodles:
         price[i] = price[i] - priced
-
+# 値下げ後価格が0以下の場合に１円にする
 for i in price:
     if price[i] <= 0:
         price[i] = 1
 
+# 表示
 print(price)
