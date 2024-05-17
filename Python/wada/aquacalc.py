83d4cee854eaedc780670fe08efe5f0729cb458f
ShioriWada <wada.shiori.lw@mynavi.jp>
2024-05-17 08:25:06 +0000

    修正

--- Python/wada/aquacalc.py
+++ Python/wada/aquacalc.py
@@ -2,13 +2,13 @@ import sys
 args = sys.argv
 
 import datetime
-from datetime import date
 
 #引数を変数に代入
 visiting_date = datetime.datetime.strptime(args[1], '%Y%m%d')
 adult_number = int(args[2])
 child_number = int(args[3])
 
+#入園料を計算して表示する関数
 def calc_entrancefee(visiting_date,adult_number,child_number):
     weekday = int(visiting_date.strftime("%w"))
     
