39c1b43965f3ef44bd1aa6d6ab6f689b4d267af6
suzuki-sota-ok <suzuki.sota.ok@mynavi.jp>
2024-05-17 05:09:47 +0000

    updatet func_oddeven.py

--- Python/suzuki/func_oddeven.py
+++ Python/suzuki/func_oddeven.py
@@ -1,4 +1,5 @@
 # 複数の整数の奇数・偶数を判定
+"""
 import sys
 # 3つの整数を定義
 num1 = int(sys.argv[1])
@@ -17,3 +18,23 @@ def calcValue(l, m, n):
             print(f"{str(i)}は奇数")
 
 calcValue(num1, num2, num3)
+
+"""
+
+# 以下、seminar assistの埋め込みコードを使ったコード
+import sys
+args = sys.argv
+
+#関数を定義
+def calcvalue(num):
+    #あまりを算出
+    mod = num % 2
+
+    #あまりの値から奇数偶数判定
+    if mod != 0:
+        print(str(num) + "は奇数")
+    else:
+        print(str(num) + "は偶数")
+
+for i in range(1,len(args),1):
+    calcvalue(int(args[i]))
