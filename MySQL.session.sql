550bb246a25c2c3ee1c84961979df0caff04ba2e
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-05-21 07:01:52 +0000

    aaa

--- MySQL.session.sql
+++ MySQL.session.sql
@@ -8,9 +8,19 @@ DESCRIBE holiday;
 
 INSERT INTO holiday (holi_date,holi_text) values
 ('2024-01-01','元旦'),
-('2024-01-08','成人の日');
+('2024-01-08','成人の日'),
 ('2024-02-11','建国記念の日'),
 ('2024-02-12','振替休日'),
 ('2024-02-23','天皇誕生日');
 
 SELECT * FROM holiday;
+
+create TABLE attendnum (
+    entry_date date not null,
+    seq int not null,
+    adult int,
+    child int,
+    primary key(entry_date,seq)
+);
+
+select * from attendnum;
