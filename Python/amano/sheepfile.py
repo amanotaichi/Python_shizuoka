0d6da8099eeec85dbca35f659ce53ce341c7d2dd
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-05-21 04:52:24 +0000

    add sheepfile.py

---
+++ Python/amano/sheepfile.py
@@ -0,0 +1,17 @@
+import sys
+
+# 引数入力
+args = sys.argv
+# 変数初期化
+count = int(args[1])
+
+# ファイルパス指定
+path = "other/sheep.txt"
+# ファイルオープン
+f = open(path, "w") 
+
+# 繰り返し
+# 指定回数文字列を出力
+for i in range(1, count+1):
+    f.write("ひつじが{0}匹\n".format(i))
+
